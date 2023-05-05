8d2d0e16c830e8ec1d9c91e044ded0b08f0740d9
mrdinall <danikardinal@yahoo.com>
2023-05-05 22:40:43 +0700

    create a comment file SparepartServiceImpl.java

--- src/main/java/com/analatar/appanalatar/service/impl/SparepartServiceImpl.java
+++ src/main/java/com/analatar/appanalatar/service/impl/SparepartServiceImpl.java
@@ -3,4 +3,5 @@ package com.analatar.appanalatar.service.impl;
 import com.analatar.appanalatar.service.SparepartService;
 
 public class SparepartServiceImpl implements SparepartService {
+    //nanti berisikan implementasi dari class interface sparepart.java
 }
