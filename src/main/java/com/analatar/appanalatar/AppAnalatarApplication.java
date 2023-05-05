5d3e202ab331cf252ace49a4cb22cce36b81f32c
mrdinall <danikardinal@yahoo.com>
2023-05-05 21:25:13 +0700

    This a first app-analatar in development

---
+++ src/main/java/com/analatar/appanalatar/AppAnalatarApplication.java
@@ -0,0 +1,13 @@
+package com.analatar.appanalatar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAnalatarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAnalatarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analatar/appanalatar/AppAnalatarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analatar.appanalatar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAnalatarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
